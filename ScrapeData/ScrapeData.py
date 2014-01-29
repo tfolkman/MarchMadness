be5265492b7ce1ddd7e5bd4bc044f12fa195f854
Syrios12 <tylerfolkman@gmail.com>
2014-01-28 23:20:17 -0800

    outputFourFactors

--- ScrapeData/ScrapeData.py
+++ ScrapeData/ScrapeData.py
@@ -1,22 +1,20 @@
 #BRING IN LIBRARIES
 
-import requests, re
+import re, csv
+from urllib2 import urlopen
 import pandas as pd
 from bs4 import BeautifulSoup
 
 #FUNCTION TO GET SOUP FROM WEBSITE
 
 def getSoup(website):
-    r = requests.get(website)
-    return BeautifulSoup(r.text)
+    r = urlopen(website)
+    return BeautifulSoup(r)
 
 #FUNCTION TO EXTRACT FOUR FACTORS FROM A SOUP
 
 def getFourFactors(soupIn):
     dataRow = []
-    colHeaders = ['Date','School1','School1_Pace','School1_eFG','School1_TOV','School1_ORB','School1_FT/FGA',
-                  'School1_ORTG','School2','School2_Pace','School2_eFG','School2_TOV','School2_ORB',
-                  'School2_FT/FGA','School2_ORTG']
     table = soupIn.find('table', {'id': 'four_factors'})
     date = soupIn.find('a', {'href': re.compile('/cbb/boxscores/index.cgi?.*')})
     tableRows = table.findAll('td')
@@ -24,6 +22,8 @@ def getFourFactors(soupIn):
     for value in tableRows:
         dataRow.append(value.text)
     return dataRow
+
+#FUNCTION TO GET LIST OF BOX SCORES ON A PAGE
 
 def getBoxScores(soupIn):
     allLinks = soupIn.findAll('a')
@@ -33,7 +33,19 @@ def getBoxScores(soupIn):
             gameLinks.append(link['href'])
     return gameLinks
 
+#FOR A GIVEN DATE GET ALL FOUR FACTORS FROM EACH BOX SCORE
+
 soup = getSoup("http://www.sports-reference.com/cbb/boxscores/index.cgi?month=11&day=12&year=2010")
 boxScores = getBoxScores(soup)
-for i in boxScores:
-    print i
+with open('./Output/FourFactors.csv','w') as f:
+    writer = csv.writer(f)
+    colHeaders = ['Date','School1','School1_Pace','School1_eFG','School1_TOV','School1_ORB','School1_FT/FGA',
+                  'School1_ORTG','School2','School2_Pace','School2_eFG','School2_TOV','School2_ORB',
+                  'School2_FT/FGA','School2_ORTG']
+    writer.writerow(colHeaders)
+    for score in boxScores[:2]:
+        scoreLink = "http://www.sports-reference.com{0}".format(score)
+        print scoreLink
+        scoreSoup = getSoup(scoreLink)
+        fourFactors = getFourFactors(scoreSoup)
+        writer.writerow(fourFactors)
