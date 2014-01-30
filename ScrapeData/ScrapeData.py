b83ca45d4875abd978a1b7776ac1de6e5987d12b
Syrios12 <tylerfolkman@gmail.com>
2014-01-29 16:29:57 -0800

    boxDateFunction

--- ScrapeData/ScrapeData.py
+++ ScrapeData/ScrapeData.py
@@ -35,17 +35,22 @@ def getBoxScores(soupIn):
 
 #FOR A GIVEN DATE GET ALL FOUR FACTORS FROM EACH BOX SCORE
 
-soup = getSoup("http://www.sports-reference.com/cbb/boxscores/index.cgi?month=11&day=12&year=2010")
-boxScores = getBoxScores(soup)
-with open('./Output/FourFactors.csv','w') as f:
-    writer = csv.writer(f)
-    colHeaders = ['Date','School1','School1_Pace','School1_eFG','School1_TOV','School1_ORB','School1_FT/FGA',
-                  'School1_ORTG','School2','School2_Pace','School2_eFG','School2_TOV','School2_ORB',
-                  'School2_FT/FGA','School2_ORTG']
-    writer.writerow(colHeaders)
-    for score in boxScores[:2]:
-        scoreLink = "http://www.sports-reference.com{0}".format(score)
-        print scoreLink
-        scoreSoup = getSoup(scoreLink)
-        fourFactors = getFourFactors(scoreSoup)
-        writer.writerow(fourFactors)
+def processDate(linkIn):
+    soup = getSoup(linkIn)
+    boxScores = getBoxScores(soup)
+    with open('./Output/FourFactors.csv','w') as f:
+        writer = csv.writer(f)
+        colHeaders = ['Date','School1','School1_Pace','School1_eFG','School1_TOV','School1_ORB','School1_FT/FGA',
+                      'School1_ORTG','School2','School2_Pace','School2_eFG','School2_TOV','School2_ORB',
+                      'School2_FT/FGA','School2_ORTG']
+        writer.writerow(colHeaders)
+        for score in boxScores:
+            scoreLink = "http://www.sports-reference.com{0}".format(score)
+            print scoreLink
+            scoreSoup = getSoup(scoreLink)
+            fourFactors = getFourFactors(scoreSoup)
+            writer.writerow(fourFactors)
+
+#PROCESS A DATE
+
+processDate("http://www.sports-reference.com/cbb/boxscores/index.cgi?month=11&day=12&year=2010")
