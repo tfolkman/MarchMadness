e122cadb5e5960b92cb4893f06db3a713c17c1fb
Syrios12 <tylerfolkman@gmail.com>
2014-01-31 14:38:20 -0800

    CleanCode

--- ScrapeData/ScrapeData.py
+++ ScrapeData/ScrapeData.py
@@ -1,73 +1,81 @@
 #BRING IN LIBRARIES
 
-import re, csv, datetime
+import re
+import csv
+import datetime
 from urllib2 import urlopen
-import pandas as pd
 from bs4 import BeautifulSoup
 
 #FUNCTION TO GET SOUP FROM WEBSITE
 
-def getSoup(website):
+
+def getsoup(website):
     r = urlopen(website)
     return BeautifulSoup(r)
 
 #FUNCTION TO EXTRACT FOUR FACTORS FROM A SOUP
 
-def getFourFactors(soupIn):
-    dataRow = []
-    table = soupIn.find('table', {'id': 'four_factors'})
-    date = soupIn.find('a', {'href': re.compile('/cbb/boxscores/index.cgi?.*')})
-    tableRows = table.findAll('td')
-    dataRow.append(date.text)
-    for value in tableRows:
-        dataRow.append(value.text)
-    return dataRow
+
+def getfourfactors(soupin):
+    datarow = []
+    table = soupin.find('table', {'id': 'four_factors'})
+    date = soupin.find('a', {'href': re.compile('/cbb/boxscores/index.cgi?.*')})
+    tablerows = table.findAll('td')
+    datarow.append(date.text)
+    for value in tablerows:
+        datarow.append(value.text)
+    return datarow
 
 #FUNCTION TO GET LIST OF BOX SCORES ON A PAGE
 
-def getBoxScores(soupIn):
-    allLinks = soupIn.findAll('a')
-    gameLinks = []
-    for link in allLinks:
+
+def getboxscores(soupin):
+    alllinks = soupin.findAll('a')
+    gamelinks = []
+    for link in alllinks:
         if link.text == "Final":
-            gameLinks.append(link['href'])
-    return gameLinks
+            gamelinks.append(link['href'])
+    return gamelinks
 
 #FOR A GIVEN DATE GET ALL FOUR FACTORS FROM EACH BOX SCORE
 
-def processDate(linkIn):
-    soup = getSoup(linkIn)
-    boxScores = getBoxScores(soup)
-    with open('./Output/FourFactors.csv','w') as f:
+
+def processdate(linkin):
+    soup = getsoup(linkin)
+    boxscores = getboxscores(soup)
+    with open('./Output/FourFactors.csv', 'w') as f:
         writer = csv.writer(f)
-        colHeaders = ['Date','School1','School1_Pace','School1_eFG','School1_TOV','School1_ORB','School1_FT/FGA',
-                      'School1_ORTG','School2','School2_Pace','School2_eFG','School2_TOV','School2_ORB',
-                      'School2_FT/FGA','School2_ORTG']
-        writer.writerow(colHeaders)
-        for score in boxScores:
-            scoreLink = "http://www.sports-reference.com{0}".format(score)
-            print scoreLink
-            scoreSoup = getSoup(scoreLink)
-            fourFactors = getFourFactors(scoreSoup)
-            writer.writerow(fourFactors)
+        colheaders = ['Date', 'School1', 'School1_Pace', 'School1_eFG', 'School1_TOV', 'School1_ORB', 'School1_FT/FGA',
+                      'School1_ORTG', 'School2', 'School2_Pace', 'School2_eFG', 'School2_TOV', 'School2_ORB',
+                      'School2_FT/FGA', 'School2_ORTG']
+        writer.writerow(colheaders)
+        for score in boxscores:
+            scorelink = "http://www.sports-reference.com{0}".format(score)
+            print scorelink
+            scoresoup = getsoup(scorelink)
+            fourfactors = getfourfactors(scoresoup)
+            writer.writerow(fourfactors)
 
 #CREATE SERIES OF DATES
 
-def dateSeries(startDate=datetime.datetime(2010,11,12),endDate=datetime.datetime.today() - datetime.timedelta(days=1)):
-    timeDelta = datetime.timedelta(days=1)
+
+def dateseries(startdate=datetime.datetime(2010, 11, 12),
+               enddate=datetime.datetime.today() - datetime.timedelta(days=1)):
+    timedelta = datetime.timedelta(days=1)
     days = []
-    while startDate < endDate:
-        days.append([startDate.strftime('%m'),startDate.strftime('%d'),startDate.strftime('%Y')])
-        startDate += timeDelta
+    while startdate < enddate:
+        days.append([startdate.strftime('%m'), startdate.strftime('%d'), startdate.strftime('%Y')])
+        startdate += timedelta
     return days
 
 #####
 
 #WORK ON GETTING THE OTHER TABLES
 
-soup = getSoup("http://www.sports-reference.com/cbb/boxscores/2014-01-29-stanford.html")
-teamTables = soup.findAll('tr', {'class': 'bold_text stat_total'})
-for table in teamTables:
-    stats = table.findAll('td')
+soup1 = getsoup("http://www.sports-reference.com/cbb/boxscores/2014-01-29-stanford.html")
+teamTables = soup1.findAll('tr', {'class': 'bold_text stat_total'})
+for table1 in teamTables:
+    stats = table1.findAll('td')
     for stat in stats:
         print stat.renderContents().strip()
+
